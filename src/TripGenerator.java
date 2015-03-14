e97cada88cd7ac4490b363eb855e451ab0a5c488
Jacob Perricone <jacobp@princeton.edu>
2015-03-14 18:12:49 -0400

    Updates

--- src/TripGenerator.java
+++ src/TripGenerator.java
@@ -7,25 +7,26 @@ import java.io.FileNotFoundException;
 
 public class TripGenerator {
 
-    public TripGenerator(String filename) {
+    public TripGenerator(File filename) {
         generateTrips(filename);
 
     }
 
-    private void generateTrips(String filename){
+    private void generateTrips(File filename){
         BufferedReader reader = null;
         String line = "";
         String csvSplitBy = ",";
         try{
             reader = new BufferedReader(new FileReader(filename));
+            reader.readLine();
             while((line = reader.readLine()) != null) {
                 String[] info = line.split(csvSplitBy);
-                int person_id = Integer.parseInt(info[5]);
-                for (int i = 1; i <= 8;i++) {
+                double person_id = Long.parseLong(info[5]);
+                for (int i = 1; i <= 7;i++) {
                     int index = (i-1)*9 + 12;
                     String oType = info[index - 5];
                     String dType = info[index - 3];
-                    if(oType == "NA" || dType == "N"){
+                    if(oType.equals("NA") || dType.equals("N")){
                         break;
                     }
                     double unique_id = createId(person_id, i);
@@ -34,15 +35,24 @@ public class TripGenerator {
                     double atime = Double.parseDouble(info[index + 2]);
                     int ox_pixel = get_x(Double.parseDouble(info[index]), Double.parseDouble(info[index + 1]));
                     int oy_pixel = get_y(Double.parseDouble(info[index]), Double.parseDouble(info[index +1]));
-                    int ax_pixel = get_x(Double.parseDouble(info[index + 9]), Double.parseDouble(info[index + 10]));
-                    int ay_pixel = get_y(Double.parseDouble(info[index + 9]), Double.parseDouble(info[index + 10 ]));
+                    int ax_pixel;
+                    int ay_pixel;
+                    if(i==7){
+                        ax_pixel = get_x(Double.parseDouble(info[12]), Double.parseDouble(info[13]));
+                        ay_pixel = get_y(Double.parseDouble(info[12]), Double.parseDouble(info[13]));
+                    }else {
+                        ax_pixel = get_x(Double.parseDouble(info[index + 9]), Double.parseDouble(info[index + 10]));
+                        ay_pixel = get_y(Double.parseDouble(info[index + 9]), Double.parseDouble(info[index + 10]));
+                    }
+
                     double oId = fetchPixelId(ox_pixel, oy_pixel);
                     double aId = fetchPixelId(ax_pixel, ay_pixel);
 
-                    System.out.println("Id: " + unique_id + " Personid: " + person_id +
-                            " Trip Number: " + i + " oxPixelId: " + oId + " aPixelID: " + aId
-                     + " oTime: " + otime + " dTime: " + dtime + " aTime " + atime + " oType: " + oType
-                    + " dType : " + dType);
+
+                    System.out.printf("Trip Number: %d, Person_id: %f, Unique Id: %f, oPixelid: %f, aPixelid: %f, oTime: %f, dTime: %f, aTime: %f, otype: %s, dType: %s \n",
+                            i, person_id, unique_id, oId, aId, otime, dtime, atime, oType, dType);
+
+
 
                 }
             }
@@ -57,8 +67,8 @@ public class TripGenerator {
         }
 
     }
-    private double createId(int a, int b) {
-        double id = 0.5*(a+b)*(a+b+1)+b;
+    private double createId(double pid, double trip) {
+        double id = pid + (trip/10);
         return id;
     }
 
@@ -77,8 +87,9 @@ public class TripGenerator {
     }
 
     public static void main(String[] args)  {
-        TripGenerator gen = new TripGenerator(args[0]);
+        //String filePath = "/Users/Tharald/IdeaProjects/TripVisualizer/src/Colorado_08001_Module6NN1stRun.csv";
+       // File inputFile = new File(filePath);
+       // TripGenerator gen = new TripGenerator(inputFile.getAbsoluteFile());
     }
 }
-
 
